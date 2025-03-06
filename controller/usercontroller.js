b4f5baf1ac2ff4d6d7710896a9bdf6fa55efa948
manthansavaliya <manthansavaliya784@gmail.com>
2025-03-06 18:44:18 +0530

    fix: some syntax

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -5,51 +5,49 @@ const jwt = require('jsonwebtoken');
 
 
 const createUser = async (req, res) => {
-    try {
-        const userdata = req.body;
-        const { name, email, password } = userdata;
-        const newPass = await bcrypt.hash(password, 10);
-        let user = new User({
-            name,
-            email,
-            password: newPass,
-        });
-        await user.save();
 
-        res.status(201).json({ message: "User Created Successfully." });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+    const userdata = req.body;
+    const { name, email, password } = userdata;
+    const newPass = await bcrypt.hash(password, 10);
+    let user = new User({
+        name,
+        email,
+        password: newPass,
+    });
+    await user.save();
+
+    res.status(201).json({ message: "User Created Successfully." });
+    res.status(400).json({ error: error.message });
 }
 const loginUser = async (req, res) => {
-    try {
-        // console.log("in Logon controller.");
-        const userdata = req.body;
-        const { email, password } = userdata;
-        // console.log(userdata.password);
+    // try {
+    // console.log("in Login controller.");
+    const userdata = req.body;
+    const { email, password } = userdata;
+    // console.log(userdata.password);
 
-        const newUser = await User.findOne({ email });
-        if (!newUser) {
-            return res.send({ error: "User Is Not Found In List." });
-        }
-        console.log(newUser);
-
-
-        const passMatch = await bcrypt.compare(password, newUser.password);
-
-        // console.log(passMatch)
-        if (!passMatch) {
-            return res.send({ error: "Password Or Details Is Invalid." });
-        }
-
-        const token = jwt.sign({ _id: newUser._id }, 'token$');
-        // console.log(token)
-
-        res.status(201).json({ message: "Login successfully", token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+    const newUser = await User.findOne({ email });
+    if (!newUser) {
+        return res.send({ error: "User Is Not Found In List." });
     }
-};
+    console.log(newUser);
+
+
+    const passMatch = await bcrypt.compare(password, newUser.password);
+
+    // console.log(passMatch)
+    if (!passMatch) {
+        return res.send({ error: "Password Or Details Is Invalid." });
+    }
+
+    const token = jwt.sign({ _id: newUser._id }, 'token$');
+    // console.log(token)
+
+    res.status(201).json({ message: "Login successfully", token });
+    // } catch (error) {
+    res.status(400).json({ error: error.message });
+}
+// };
 
 const ping = async function (req, res) {
     try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createUser, loginUser, ping } = require('../controller/usercontroller.js');
 const { jwtAuth } = require('../middleware/jwt.js');
+const tryCatch = require('../middleware/tryCatch.js');
 const router = express.Router();
 
 
-router.post('/create', createUser);
-router.post('/login', loginUser);
+router.post('/create', tryCatch(createUser));
+router.post('/login', tryCatch(loginUser));
 
 
 router.post('/ping', jwtAuth, ping);
